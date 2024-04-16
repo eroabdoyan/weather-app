3326be36ee930a8fd1b7d61ee9b367fcdb89709c
Ero <abdoyan2004@mail.ru>
2024-04-16 20:54:24 +0400

    Add HTTPS support for weather API requests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-
 import { useEffect, useState } from "react";
 import TempInfo from "./components/TempInfo/TempInfo";
 import axios from "axios";
@@ -35,23 +34,21 @@ const Home = () => {
       try {
         if(inputLocation === '') {
           if (location.lat !== null && location.lon !== null) {
-            const currentWeatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location.lat},${location.lon}`);
+            const currentWeatherResponse = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location.lat},${location.lon}`);
             setCurrentWeather(currentWeatherResponse.data);
 
-            const forecastResponse = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location.lat},${location.lon}&days=7`);
+            const forecastResponse = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location.lat},${location.lon}&days=7`);
             setDailyForecast(forecastResponse.data.forecast.forecastday);
             setHourlyForecast(forecastResponse.data.forecast.forecastday[0].hour);
           }
         }else {
-          const currentWeatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${inputLocation}`);
+          const currentWeatherResponse = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${inputLocation}`);
           setCurrentWeather(currentWeatherResponse.data);
 
-          const forecastResponse = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${inputLocation}&days=7`);
+          const forecastResponse = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${inputLocation}&days=7`);
           setDailyForecast(forecastResponse.data.forecast.forecastday)
           setHourlyForecast(forecastResponse.data.forecast.forecastday[0].hour);
         }
-        
-        
       } catch (error) {
         console.error('Error fetching weather data:', error);
       }
@@ -88,7 +85,6 @@ const Home = () => {
 
   return (
     <main className="flex justify-center items-center text-white my-1">
-      
       <div className="p-2 rounded-3xl border-white w-[1000px] bg-cover"  style={{ backgroundImage: `url('${background}')` }}>
         <div className="mt-5">
           <Search 
